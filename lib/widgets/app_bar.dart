d0891140f18a3af1c5e1a1c7b3725cb8adc4c081
Swaranjana Nayak <swaranjananayak@gmail.com>
2022-12-29 23:22:41 +0530

    app bar widget

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:metops/utils/colors.dart';
+
+class AppBarCustom extends StatefulWidget with PreferredSizeWidget {
+  const AppBarCustom({super.key});
+
+  @override
+  State<AppBarCustom> createState() => _AppBarCustomState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class _AppBarCustomState extends State<AppBarCustom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 20, right: 10),
+      child: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: neutral,
+        ),
+        backgroundColor: neutral,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: primary,
+          size: 30,
+        ),
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.account_circle_outlined,
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
