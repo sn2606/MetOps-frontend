944f053a91d00579a1eeaf3eddeb01ec29679112
Swaranjana Nayak <swaranjananayak@gmail.com>
2022-12-31 22:41:50 +0530

    query model

---
+++ lib/models/queryModel.dart
@@ -0,0 +1,11 @@
+class QueryModel {
+  String id;
+  double latitude;
+  double longitude;
+
+  QueryModel({
+    required this.id,
+    required this.latitude,
+    required this.longitude,
+  });
+}
