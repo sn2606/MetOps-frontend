a6eafa6ee6e33330610ba0d103450fdf0c09ab00
Swaranjana Nayak <swaranjananayak@gmail.com>
2022-12-31 22:34:58 +0530

    record model

---
+++ lib/models/record.dart
@@ -0,0 +1,25 @@
+class Record {
+  static const altitude = 20000;
+  static const interval = 100;
+  double latitude;
+  double longitude;
+  String? location;
+  List<double> actualTemperature;
+  List<double> virtualTemperature;
+  List<double> pressure;
+  List<double> humidity;
+  List<double> windSpeed;
+  List<double> windDirection;
+
+  Record({
+    this.location,
+    required this.latitude,
+    required this.longitude,
+    required this.actualTemperature,
+    required this.humidity,
+    required this.pressure,
+    required this.virtualTemperature,
+    required this.windDirection,
+    required this.windSpeed,
+  });
+}
