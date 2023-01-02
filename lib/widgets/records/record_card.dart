df36b5de1875ef3a26a16eede3e9095a60a0e79f
Swaranjana Nayak <swaranjananayak@gmail.com>
2023-01-02 18:01:04 +0530

    make records tappable

--- lib/widgets/records/record_card.dart
+++ lib/widgets/records/record_card.dart
@@ -12,78 +12,81 @@ class RecordCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-        side: BorderSide(color: primary),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-        child: Container(
-          decoration: BoxDecoration(
-            color: neutralContainer,
-            border: Border(
-              left: BorderSide(
-                color: primary,
-                width: 5,
+    return InkWell(
+      onTap: () {},
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          side: BorderSide(color: primary),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          child: Container(
+            decoration: BoxDecoration(
+              color: neutralContainer,
+              border: Border(
+                left: BorderSide(
+                  color: primary,
+                  width: 5,
+                ),
               ),
             ),
-          ),
-          padding: EdgeInsets.all(10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.location_on_outlined,
-                    color: primary,
-                    size: 30,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${recordsList[index].latitude}, ${recordsList[index].longitude}',
-                        style: TextStyle(
-                          color: primary,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        '${recordsList[index].location}',
-                        style: TextStyle(
-                          color: primary,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    DateFormat('dd-MM-yy').format(recordsList[index].created),
-                    style: TextStyle(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
                       color: primary,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
+                      size: 30,
                     ),
-                  ),
-                  Text(
-                    DateFormat.jm().format(recordsList[index].created),
-                    style: TextStyle(
-                      color: primary,
-                      fontSize: 12,
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${recordsList[index].latitude}, ${recordsList[index].longitude}',
+                          style: TextStyle(
+                            color: primary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          '${recordsList[index].location}',
+                          style: TextStyle(
+                            color: primary,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      DateFormat('dd-MM-yy').format(recordsList[index].created),
+                      style: TextStyle(
+                        color: primary,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.jm().format(recordsList[index].created),
+                      style: TextStyle(
+                        color: primary,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
