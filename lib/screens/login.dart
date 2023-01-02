d95b4900a07164151596f2d74dab156c5fd22215
Swaranjana Nayak <swaranjananayak@gmail.com>
2023-01-02 23:21:57 +0530

    login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../utils/colors.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: neutral,
+        systemNavigationBarColor: neutral,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarIconBrightness:
+            Brightness.dark, //navigation bar icons' color
+      ),
+      child: Container(
+        child: Scaffold(),
       ),
     );
   }
