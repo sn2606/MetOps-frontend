a7a9ded57c1f41038ccbe0e9236ac7a780411726
Swaranjana Nayak <swaranjananayak@gmail.com>
2023-01-04 22:57:30 +0530

    remove inkwell, add iconbutton

--- lib/widgets/scaffold/app_bar.dart
+++ lib/widgets/scaffold/app_bar.dart
@@ -32,25 +32,16 @@ class _AppBarCustomState extends State<AppBarCustom> {
         ),
         actions: [
           Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-            ),
             padding: EdgeInsets.only(right: 20.0),
-            child: InkWell(
-              customBorder: const CircleBorder(),
-              onTap: () {
+            child: IconButton(
+              onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => Account()),
                 );
               },
-              child: Ink(
-                decoration: const BoxDecoration(shape: BoxShape.circle),
-                height: 50,
-                width: 50,
-                child: const Icon(
-                  Icons.settings_outlined,
-                ),
+              icon: const Icon(
+                Icons.settings_outlined,
               ),
             ),
           ),
