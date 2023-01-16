b85f0174388e628b94bc5db85a258b95552edaf0
Swaranjana Nayak <swaranjananayak@gmail.com>
2023-01-16 13:37:26 +0530

    login service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,34 +1,78 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_session_manager/flutter_session_manager.dart';
 import 'package:http/http.dart' as http;
 
 import '../utils/api.dart';
+import '../models/user.dart';
 
 class AuthService {
   // ignore: non_constant_identifier_names
   static final SESSION = SessionManager();
 
-  Future<dynamic> login(String username, String password) async {
+  Future<Map<String, dynamic>> login(String username, String password) async {
     try {
-      final queryParameters = {'username': username, 'password': password};
-      final uri =
-          Uri.http(Endpoints.authority, Endpoints.login, queryParameters);
+      final queryParameters = jsonEncode({
+        'username': username,
+        'password': password,
+      });
+      final uri = Uri.http(Endpoints.authority, Endpoints.login);
       http.Response response = await http.post(
         uri,
         headers: {
-          HttpHeaders.acceptHeader: 'application/json',
           HttpHeaders.contentTypeHeader: 'application/json',
         },
+        body: queryParameters,
       );
 
-      print('boo');
+      if (response.statusCode == 200) {
+        final tokenMap = Map<String, dynamic>.from(jsonDecode(response.body));
+        final tokens = Map<String, String>.from(tokenMap['token']);
+        final accessToken = tokens['access'];
+        final refreshToken = tokens['refresh'];
+        setToken(accessToken!, refreshToken!);
 
-      print(response.body);
+        // get account info of logged in user
+        final accUri = Uri.http(Endpoints.authority, Endpoints.accountInfo);
+        http.Response accInfoResponse = await http.get(
+          accUri,
+          headers: {
+            HttpHeaders.contentTypeHeader: 'application/json',
+            HttpHeaders.acceptHeader: 'application/json',
+            HttpHeaders.authorizationHeader: "Bearer $accessToken"
+          },
+        );
 
-      return response.body;
-    } finally {
-      // you can do somethig here
+        if (accInfoResponse.statusCode == 200) {
+          final accInfo =
+              Map<String, dynamic>.from(jsonDecode(accInfoResponse.body));
+          setAccInfo(accInfo['id']!, accInfo['username']!);
+          User loggedIn = User(
+            id: accInfo['id'],
+            username: accInfo['username'],
+            fullName: accInfo['full_name'],
+            email: accInfo['email'],
+            phoneNo: accInfo['phone_no'],
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+          );
+
+          return {
+            'status': 200,
+            'user': loggedIn,
+          };
+        } else {
+          throw Exception(response.statusCode);
+        }
+      } else {
+        throw Exception(response.statusCode);
+      }
+    } catch (error) {
+      return {
+        'status': 400,
+        'error': error,
+      };
     }
   }
 
@@ -38,8 +82,24 @@ class AuthService {
     await SESSION.set('tokens', data);
   }
 
+  static setAccInfo(int id, String username) async {
+    await SESSION.set('userid', id);
+    await SESSION.set('username', username);
+  }
+
   static Future<Map<String, dynamic>> getToken() async {
     return await SESSION.get('tokens');
+  }
+
+  static Future<Map<String, dynamic>> getAccInfo() async {
+    int userid = await SESSION.get('userid');
+    String username = await SESSION.get('username');
+    Map<String, dynamic> info = {
+      'userid': userid,
+      'username': username,
+    };
+
+    return info;
   }
 
   static removeToken() async {
--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,5 +1,6 @@
 class Endpoints {
   static const String authority = '10.0.2.2:8000';
   static const String askQuery = '/api/query/';
-  static const String login = '/api/login/';
+  static const String login = '/api/users/login/';
+  static const String accountInfo = 'api/users/account/';
 }
