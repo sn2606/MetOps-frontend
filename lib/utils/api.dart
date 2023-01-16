8e5a1856675f10abd2176f396616897ce91aba05
Swaranjana Nayak <swaranjananayak@gmail.com>
2023-01-16 17:54:02 +0530

    query result save and cancel

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,6 +1,7 @@
 class Endpoints {
   static const String authority = '10.0.2.2:8000';
   static const String askQuery = '/api/query/';
+  static const String saveQuery = '/api/query/save/';
   static const String login = '/api/users/login/';
   static const String accountInfo = 'api/users/account/';
 }
--- lib/widgets/query/table_builder.dart
+++ lib/widgets/query/table_builder.dart
@@ -1,7 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_session_manager/flutter_session_manager.dart';
+import 'package:http/http.dart' as http;
 
 import '../../models/record.dart';
+import '../../utils/api.dart';
 import '../../utils/styles.dart';
+import '../../utils/router.dart';
+import '../../screens/main_screen.dart';
 import '../action_btn_filled.dart';
 import '../action_btn_outlined.dart';
 
@@ -9,12 +17,61 @@ class TableBuilder extends StatelessWidget {
   final Future<List<RecordItem>?> futureRecordItems;
   final double latitude;
   final double longitude;
+
   const TableBuilder({
     super.key,
     required this.futureRecordItems,
     required this.latitude,
     required this.longitude,
   });
+
+  void _save(BuildContext context, VoidCallback ifOk) async {
+    final session = SessionManager();
+    final tokens = await session.get('tokens');
+    final accessToken = tokens['accessToken'];
+
+    List<RecordItem>? records = await futureRecordItems;
+    final Map<String, dynamic> saveBody = {
+      'user': {
+        'userid': await session.get('userid'),
+      },
+      'query': {
+        'latitude': latitude,
+        'longitude': longitude,
+      },
+      'records': records!.map((rec) {
+        return {
+          'height': rec.height,
+          'temperature': double.parse(rec.actualTemperature.toStringAsFixed(4)),
+          'virtual_temperature':
+              double.parse(rec.virtualTemperature.toStringAsFixed(4)),
+          'pressure': double.parse(rec.pressure.toStringAsFixed(4)),
+          'relative_humidity':
+              double.parse(rec.relativeHumidity.toStringAsFixed(4)),
+          'wind_speed': double.parse(rec.windSpeed.toStringAsFixed(4)),
+          'wind_direction': double.parse(rec.windDirection.toStringAsFixed(4)),
+        };
+      }).toList()
+    };
+
+    // save the above constructed data
+    final uri = Uri.http(Endpoints.authority, Endpoints.saveQuery);
+    http.Response response = await http.post(
+      uri,
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+        HttpHeaders.acceptHeader: 'application/json',
+        HttpHeaders.authorizationHeader: "Bearer $accessToken"
+      },
+      body: jsonEncode(saveBody),
+    );
+
+    if (response.statusCode == 200) {
+      ifOk();
+    } else {
+      // show error - state management
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -102,11 +159,21 @@ class TableBuilder extends StatelessWidget {
                 children: [
                   ActionButtonFilled(
                     title: 'Save',
-                    onPressed: () {},
+                    onPressed: () => _save(context, () {
+                      Navigate.pushPage(
+                        context,
+                        const ScaffoldCustom(screenIndex: 2),
+                      );
+                    }),
                   ),
                   ActionButtonOutlined(
                     title: 'Cancel',
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigate.pushPage(
+                        context,
+                        const ScaffoldCustom(screenIndex: 1),
+                      );
+                    },
                   )
                 ],
               ),
