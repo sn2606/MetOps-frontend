82a4abc3ca75a89a623f7d2e6bd9d051848b7fae
Swaranjana Nayak <swaranjananayak@gmail.com>
2023-01-17 16:39:42 +0530

    api for save

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,7 +1,8 @@
 class Endpoints {
   static const String authority = '10.0.2.2:8000';
   static const String askQuery = '/api/query/';
+  static const String queryList = '/api/query/list';
   static const String saveQuery = '/api/query/save/';
   static const String login = '/api/users/login/';
-  static const String accountInfo = 'api/users/account/';
+  static const String accountInfo = '/api/users/account/';
 }
